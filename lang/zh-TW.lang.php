72af239ff685c81965f73a3fc66773c22288186d
royallin <royallin@7db839d3-7499-e74c-7ded-2c83870bfeb4>
2010-02-01 06:00:39 +0000

    Update TW (Traditional Chinese) language
    git-svn-id: http://xe-board.googlecode.com/svn/modules/board@16 7db839d3-7499-e74c-7ded-2c83870bfeb4

--- lang/zh-TW.lang.php
+++ lang/zh-TW.lang.php
@@ -15,7 +15,6 @@
     $lang->thumbnail = '縮圖';
     $lang->last_post = '最新發表';
 
-
     // 項目
     $lang->search_result = '搜尋結果';
     $lang->consultation = '咨詢功能';
@@ -30,10 +29,10 @@
     $lang->cmd_list_setting = '列表設定';
 
     // 其他
-    $lang->about_layout_setup = '可直接編輯部落格版面設計原始碼。可把Widget原始碼插入到您喜歡的位置。';
+    $lang->about_layout_setup = '可直接編輯部落格版面設計原始碼。可把 Widget 原始碼插入到您喜歡的位置。';
     $lang->about_board_category = '可以新增/刪除分類項目<br />分類有異常情況時，可以嘗試重新建立暫存檔。';
     $lang->about_except_notice = '設置公告列表項目，不再重複顯示到普通列表當中。';
-    $lang->about_use_anonymous = '討論板可使用匿名功能。可在面板設定中設置隱藏作者的資料。';
+    $lang->about_use_anonymous = '討論板可使用匿名功能。可在面板設定中設置隱藏作者的資料。 ';
     $lang->about_board = '可建立及管理討論板的模組。';
     $lang->about_consultation = "咨詢功能是指除有管理權限的會員以外，其他會員只能瀏覽自己發表的主題。\n使用咨詢功能時系統將自動禁止非會員的發表主題權限。";
     $lang->about_secret = '可用於討論板或回覆時選擇是否使用。';
