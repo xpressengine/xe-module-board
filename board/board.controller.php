bfa00fb3005f48b79a5d0065be305b852fbd60d9
haneul <syhan@cs.washington.edu>
2010-05-17 09:34:27 +0000

    #18876232 : change written article to use nick_name instead of user id
    git-svn-id: http://xe-board.googlecode.com/svn/modules@22 7db839d3-7499-e74c-7ded-2c83870bfeb4

--- board/board.controller.php
+++ board/board.controller.php
@@ -329,7 +329,7 @@
             if(!$member_info->user_id) return new Object();
 
             // 아이디로 검색기능 추가
-            $url = getUrl('','mid',$mid,'search_target','user_id','search_keyword',$member_info->user_id);
+            $url = getUrl('','mid',$mid,'search_target','nick_name','search_keyword',$member_info->nick_name);
             $oMemberController = &getController('member');
             $oMemberController->addMemberPopupMenu($url, 'cmd_view_own_document', './modules/member/tpl/images/icon_view_written.gif');
 
