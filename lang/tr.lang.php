18546745a1dac8adab62bf30617a0585c5f2822b
ngleader <ngleader@gmail.com>
2011-01-25 05:46:45 +0000

    add Turkey langauge
    git-svn-id: http://xe-board.googlecode.com/svn/modules/board@72 7db839d3-7499-e74c-7ded-2c83870bfeb4

---
+++ lang/tr.lang.php
@@ -0,0 +1,48 @@
+<?php
+    /**
+     * @file   en.lang.php
+     * @author zero (zero@nzeo.com)
+     * @brief  board modules's basic language pack
+     **/
+
+    $lang->board = 'Pano';
+
+    $lang->except_notice = 'Bildirimler Harici';
+    $lang->use_anonymous = 'Anonim Kullan';
+
+    $lang->cmd_manage_menu = 'Menüleri Yönet';
+    $lang->list_target_item = 'Hedef Parça';
+    $lang->list_display_item = 'Parçayı Göster';
+    $lang->summary = 'Özet';
+    $lang->thumbnail = 'Küçük Resim';
+    $lang->last_post = 'Son Gönderi';
+
+    // Item
+    $lang->search_result = 'Arama Sonuçları';
+    $lang->consultation = 'Başvuru';
+    $lang->secret = 'Gizli';
+    $lang->thisissecret = 'Bu gizli bir makaledir.';
+    $lang->admin_mail = "Yönetici E-postası";
+
+    // words used in button
+    $lang->cmd_board_list = 'Pano Listesi';
+    $lang->cmd_module_config = 'Genel Pano Ayarları';
+    $lang->cmd_view_info = 'Pano Bilgisi';
+    $lang->cmd_list_setting = 'Liste Ayarları';
+    $lang->cmd_create_board = 'Yeni pano oluştur';
+
+    // blah blah..
+    $lang->about_layout_setup = 'Pano yerleşim düzeni kodunu kendiniz değiştirebilirsiniz. Grafik bileşeni kodunu, istediğiniz yere yerleştirebilir veya yönetebilirsiniz.';
+    $lang->about_board_category = 'Pano kategorileri oluşturabilirsiniz.<br />Pano kategorisi bozulduğunda, önbellek dosyasını yeniden kurmayı deneyiniz.';
+    $lang->about_except_notice = 'Bildiri makaleleri normal listede görüntülenmeyecektir.';
+    $lang->about_use_anonymous = 'Bu panoyu, yazar bilgisini saklayarak anonim pano haline dönüştür.<br /><strong>Lütfen ek ayarlardan geçmiş\'i kapatınız. Eğer kapatmazsanız, belgeyi değiştirmek yazar bilgisinin görüntülenmesine neden olacaktır.</strong>';
+    $lang->about_board = 'Bu modül, pano oluşturmak ve yönetmek içindir.';
+    $lang->about_consultation = "Yönetici olmayan üyeler, kendi makalelerini görebileceklerdir.\nÜye olmayan kullanıcılar başvuruyu kullanıp, makale yazamayacaklardır.";
+    $lang->about_secret = 'Kullanıcılar gizli makaleler veya gizli yorumlar yazabileceklerdir.';
+    $lang->about_admin_mail = 'Makale ya da yorum gönderildiğinde e-posta gönderilecektir.<br />Çoklu mailler noktalarla(,) ayrılıp gönderilecektir.';
+    $lang->about_list_config = 'Eğer liste-tarzı dış görünüm kullanılıyorsa, gösterilecek parçaları düzenleyebilirsiniz.<br />Bununla birlikte, bu özellik resmi olmayan dış görünümler için mevcut olmayabilir.<br />Eğer hedef parçaya çift tıklarsanız ve parçalar gösterilirse, ekle / çıkar özelliklerini kullanabilirsiniz.';
+
+    $lang->msg_not_enough_point = 'Bu panoya makele yazmak için yeterli puanınız bulunmamaktadır.';
+
+	$lang->write_comment = 'Yorum Yaz';
+?>
