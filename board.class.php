4fa5356d4f2282bafa2bf94275ab2bf372590f7a
flyskyko <sinsy200@gmail.com>
2012-02-24 09:59:26 +0000

    issue 6, added 'blamed_count' on list
    git-svn-id: http://xe-board.googlecode.com/svn/branches/1.4@167 7db839d3-7499-e74c-7ded-2c83870bfeb4

--- board.class.php
+++ board.class.php
@@ -9,7 +9,7 @@
 
         var $search_option = array('title','content','title_content','comment','user_name','nick_name','user_id','tag'); ///< 검색 옵션
 
-        var $order_target = array('list_order', 'update_order', 'regdate', 'voted_count', 'readed_count', 'comment_count', 'title'); // 정렬 옵션
+        var $order_target = array('list_order', 'update_order', 'regdate', 'voted_count', 'blamed_count', 'readed_count', 'comment_count', 'title'); // 정렬 옵션
 
         var $skin = "default"; ///< 스킨 이름
         var $list_count = 20; ///< 한 페이지에 나타날 글의 수
