da6ffb754d818da70be74fb27241ea378d2d8e22
bnu <bnufactory@gmail.com>
2013-11-30 19:48:09 +0900

    me2day 공유 기능 제거.

--- skins/default/board.default.js
+++ skins/default/board.default.js
@@ -48,10 +48,6 @@ jQuery(function($){
 		opts = $.extend({}, {type:'twitter', event:'click', content:''}, opts);
 		opts.content = encodeURIComponent(opts.content);
 		switch(opts.type) {
-			case 'me2day':
-				loc = 'http://me2day.net/posts/new?new_post[body]='+opts.content;
-				if (opts.tag) loc += '&new_post[tags]='+encodeURIComponent(opts.tag);
-				break;
 			case 'facebook':
 				loc = 'http://www.facebook.com/share.php?t='+opts.content+'&u='+encodeURIComponent(opts.url||location.href);
 				break;
